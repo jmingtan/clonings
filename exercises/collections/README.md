c8fac53ccb41f0ce59b70076d3a815d7b2ec13e5
Eugene Tan <contact@eugene.io>
2019-08-31 00:09:09 +0800

    add collection exercise

---
+++ exercises/collections/README.md
@@ -0,0 +1,13 @@
+# Collections
+
+One of the core tenets of Clojure is ["polymorphism"](https://clojure.org/about/rationale#_object_orientation_is_overrated). One example of this principle is that many functions in the core library operate on a common set of abstractions (interfaces).
+
+Here are some of the primary collections in Clojure:
+
+- seq
+- list
+- map
+- vector
+- set
+
+https://clojure.org/reference/data_structures#Collections
---
+++ exercises/collections/collections1.clj
@@ -0,0 +1,42 @@
+;; collections1.clj
+;; Make me compile! Scroll down for hints :)
+
+(ns collections.collections1)
+
+(defn -main []
+  (if (coll? ???)
+    (println "That's a great looking collection you've got there!")
+    (println "Hmm, this doesn't look like any collection I've seen!")))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;; Try creating a collection using any of Clojure's collection literals (e.g. for vector, list, map, etc)
